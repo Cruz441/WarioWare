5ba72bcfa770555fda9813834e2f35885e30e2fe
Cruz441 <Cruz441@users.noreply.github.com>
2018-11-30 13:31:19 -0500

    ll

---
+++ Assets/CCScripts/CCPlayerController.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class Boundary
+{
+public float xMin, xMax, yMin, yMax;
+}
+
+
+public class CCPlayerController : MonoBehaviour {
+
+    private Rigidbody2D rb;
+    public float speed;
+    private float moveInput;
+
+ 
+
+    //--text variables--//
+    public Text countText;    
+    public Text endText;
+    private int count;
+
+    public Boundary boundary;
+    private float timer;
+    private object GameLoader;
+
+    //--Sound--//
+    public AudioClip snowFlakeCaught;
+    public AudioClip enemyCaught;
+
+    void Start()
+    {
+        NewMethod();
+        count = 0;        
+        endText.text = "";
+        SetCountText();
+        //SetIntroText();
+    }
+
+    private void NewMethod()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        float moveVertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal,moveVertical,0.0f);
+        rb.velocity = movement * speed;
+
+        rb.position = new Vector3
+            (
+            Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
+            Mathf.Clamp(rb.position.y, boundary.yMin, boundary.yMax),
+            0.0f
+            );
+
+        //This does a timer before ending the game after 10 seconds.
+        timer = timer + Time.deltaTime;
+        if (timer >= 10)
+        {
+            endText.text = "You Lose! :(";
+            StartCoroutine(ByeAfterDelay(2));
+
+        }
+    }
+
+    IEnumerator ByeAfterDelay(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        // Code to execute after the delay
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+            AudioSource audio = GetComponent<AudioSource>();
+            audio.PlayOneShot(snowFlakeCaught);
+            count = count + 1;
+            SetCountText();
+
+          
+        }
+        else if (other.gameObject.CompareTag("Enemy"))
+        {
+            other.gameObject.SetActive(false);
+
+            AudioSource audio = GetComponent<AudioSource>();
+            audio.PlayOneShot(enemyCaught);
+            endText.text = "You Lose! :(";
+            StartCoroutine(ByeAfterDelay(2));
+        }
+    }
+   
+    void SetCountText()
+    {
+        countText.text = "Caught: " + count.ToString();
+        if (count >= 10)
+        {
+            endText.text = "You win!";
+            StartCoroutine(ByeAfterDelay(2));
+        }
+    }
+}
---
+++ Assets/CCScripts/CCSnowMover.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CCSnowMover : MonoBehaviour {
+
+    public GameObject[] snowflakes;
+    public Vector3 spawnValue;
+
+
+    public int snowFlakeCount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    IEnumerator SpawnSnowflakes()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            for (int i = 0; i < snowFlakeCount; i++)
+            {
+                GameObject snowflake = snowflakes[Random.Range(0, snowflakes.Length)];
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValue.x, spawnValue.x), spawnValue.y, spawnValue.z);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(snowflake, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+            }
+        }
+
+    }
+}
